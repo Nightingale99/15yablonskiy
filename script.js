3b3ad8d327f4eddf0f357c790a78b5143e95727d
Nightingale99 <113011456+Nightingale99@users.noreply.github.com>
2023-09-13 21:35:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const width = window.innerWidth
-    if (width < 768){
-        const swiper = new Swiper('.swiper-container', {
-            // Optional parameters
-            direction: 'horizontal',
-            slidesPerView: 'auto', // Автоматическое определение количества видимых слайдов
-            spaceBetween: 10, // Отступ между слайдами (замените на свой желаемый отступ)
-            loop: true,
-          
-            // If we need pagination
-            pagination: {
-              el: '.swiper-pagination',
-            },
-          
-            // Navigation arrows
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          
-            // And if we need scrollbar
-            scrollbar: {
-              el: '.swiper-scrollbar',
-            },
-        });
-    }
-  })
+            const swiper = new Swiper('.swiper-container', {
+                // Optional parameters
+                direction: 'horizontal',
+                slidesPerView: 'auto', // Автоматическое определение количества видимых слайдов
+                spaceBetween: 10, // Отступ между слайдами (замените на свой желаемый отступ)
+                loop: true,
+              
+                // If we need pagination
+                pagination: {
+                  el: '.swiper-pagination',
+                },
+                breakpoints: {
+                  767 : {
+                    enabled: false,
+                  }
+                },
+            });
 
 let moreBrands = document.querySelector('.swiper-container__more-brands');
 let classCheckout = 0;
